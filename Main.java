b539f54ea8363a9f79d8f9c6fcb8b1ccb614c5e8
Marcelo Eugênio <123710944+CeloEug@users.noreply.github.com>
2024-03-18 09:04:24 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         Vetor lista = new Vetor();
 
         System.out.printf("Hello and welcome!");
